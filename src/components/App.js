fd21f66208973d95f5bf5db4c2d981610913e399
Violet Cutler <violetcutler@Violets-MacBook-Air.local>
2022-08-24 17:03:19 -0400

    added a basic single product view

--- src/components/App.js
+++ src/components/App.js
@@ -1,16 +1,24 @@
-import React from "react";
-import { Navbar, Homepage, AllProducts, Phones } from "./index";
+import React, { useState, useEffect } from "react";
+import { Navbar, Homepage, AllProducts, SingleProduct, Phones } from "./index";
 import { Routes, Route } from "react-router-dom";
-
+import { getAllProducts } from "../api";
 
 const App = () => {
+  const [products, setProducts] = useState([]);
+
+  useEffect(() => {
+    getAllProducts().then((fetchedProducts) => setProducts(fetchedProducts));
+  }, []);
+
   return (
     <div id="appContainer">
       <Navbar />
       <Routes>
-        <Route path="/" element={<Homepage/>} />
-        <Route path="/products" element={<AllProducts />} />
-        <Route path="/products/phones" element={<Phones />}/>
+        <Route path="/" element={<Homepage />} />
+        <Route path="/products" element={<AllProducts products={products} />} />
+        <Route path="/products/:id" element={<SingleProduct
+        products={products}/>}/>
+        <Route path="/products/phones" element={<Phones />} />
       </Routes>
     </div>
   );
